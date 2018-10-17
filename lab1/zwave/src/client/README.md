a52407ff22b9e5a9477221df32e1ac467f541cfb
raedAbr <abdennadher.raed@gmail.com>
2018-10-17 23:31:04 +0200

    Update the zwave client Add the zip file to render (lab1_part2)

--- lab1/zwave/src/client/README.md
+++ lab1/zwave/src/client/README.md
@@ -1,9 +1,14 @@
-* Python version: 
+* Python version: 3
 * Required packaged: sleep, requests and flask
+
+> Be sure that the server is running before running the client because we did not manage the case where the server is down.
+
+> By default, we put `192.168.1.2` as server ip and `5000` as server port. These values can be set in the home page of the client by clicking on the `Server config` button.
+
 * To run the client:
 
 ```shell
 $ python3 client.py
 ```
 
-the application will be running on `http://localhost:8080/`
+the application will runs on `http://localhost:3001/`
--- lab1/zwave/src/client/client.py
+++ lab1/zwave/src/client/client.py
@@ -4,17 +4,33 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+server_ip = '192.168.1.2'
+server_port = 5000
+
+
+@app.route('/server_config.html', methods=['GET', 'POST'])
+def server_config():
+    if request.method == 'POST':
+        global server_ip
+        global server_port
+        server_ip = request.form['inputIp']
+        server_port = int(request.form['inputPort'])
+        print('Changing server to ' + server_ip + ':' + str(server_port))
+        return render_template('index.html')
+
+    return render_template('server_config.html')
+
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route('/network.html', methods=['GET', 'POST'])
+@app.route('/network.html', methods=['GET'])
 def network():
-    network_info_obj = requests.get('http://192.168.1.2:5000/network/info').json()
-    return render_template("network.html", network=network_info_obj, network_size=len(network_info_obj)-1)
+    network_info_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/network/info').json()
+    return render_template('network.html', network=network_info_obj, network_size=len(network_info_obj)-1)
 
 
 @app.route('/networkConfig.html', methods=['GET', 'POST'])
@@ -24,40 +40,40 @@ def network_config():
         group1_report = request.form['group1Report']
         wakeup_interval = request.form['wakeupInterval']
         requests.post(
-            'http://192.168.1.2:5000/network/set_sensor_nodes_basic_configuration',
+            'http://' + server_ip + ':' + str(server_port) + '/network/set_sensor_nodes_basic_configuration',
             json={
-                "Group_Interval": group1_interval,
-                "Group_Reports": group1_report,
-                "Wake-up_Interval": wakeup_interval
+                'Group_Interval': group1_interval,
+                'Group_Reports': group1_report,
+                'Wake-up_Interval': wakeup_interval
             }
         )
         sleep(5)
 
-    network_obj = requests.get('http://192.168.1.2:5000/network/get_nodes_configuration').json()
-    return render_template("networkConfig.html", network=network_obj, network_size=len(network_obj)-1)
+    network_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/network/get_nodes_configuration').json()
+    return render_template('networkConfig.html', network=network_obj, network_size=len(network_obj)-1)
 
 
 @app.route('/nodes.html')
 def nodes():
-    nodes_obj = requests.get('http://192.168.1.2:5000/nodes/get_nodes_list').json()
-    return render_template("nodes.html", nodes=nodes_obj, nodes_len=len(nodes_obj))
+    nodes_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/nodes/get_nodes_list').json()
+    return render_template('nodes.html', nodes=nodes_obj, nodes_len=len(nodes_obj))
 
 
 @app.route('/sensors.html')
 def sensors():
-    sensors_obj = requests.get('http://192.168.1.2:5000/sensors/get_sensors_list').json()
-    return render_template("sensors.html", nodes=sensors_obj, nodes_len=len(sensors_obj))
+    sensors_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/get_sensors_list').json()
+    return render_template('sensors.html', nodes=sensors_obj, nodes_len=len(sensors_obj))
 
 
 @app.route('/nodeMeasures.html')
 def sensor_measures():
-    node_id = request.args.get("nodeid", 0)
+    node_id = request.args.get('nodeid', 0)
     if node_id != 0:
-        node_temperature = requests.get('http://192.168.1.2:5000/sensors/' + str(node_id) + '/get_temperature').json()
-        node_humidity = requests.get('http://192.168.1.2:5000/sensors/' + str(node_id) + '/get_humidity').json()
-        node_luminance = requests.get('http://192.168.1.2:5000/sensors/' + str(node_id) + '/get_luminance').json()
-        node_motion = requests.get('http://192.168.1.2:5000/sensors/' + str(node_id) + '/get_motion').json()
-        node_all_measures = requests.get('http://192.168.1.2:5000/sensors/' + str(node_id) + '/get_all_measures').json()
+        node_temperature = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/' + str(node_id) + '/get_temperature').json()
+        node_humidity = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/' + str(node_id) + '/get_humidity').json()
+        node_luminance = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/' + str(node_id) + '/get_luminance').json()
+        node_motion = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/' + str(node_id) + '/get_motion').json()
+        node_all_measures = requests.get('http://' + server_ip + ':' + str(server_port) + '/sensors/' + str(node_id) + '/get_all_measures').json()
         node = {
             'id': node_id,
             'temperature': node_temperature,
@@ -66,55 +82,55 @@ def sensor_measures():
             'motion': node_motion,
             'all_measures': node_all_measures
         }
-        return render_template("nodeMeasures.html", node=node)
+        return render_template('nodeMeasures.html', node=node)
 
 
-@app.route('/actuatorLevel.html', methods=["GET", "POST"])
+@app.route('/actuatorLevel.html', methods=['GET', 'POST'])
 def actuator_level():
-    node_id = request.args.get("nodeid", 0)
-    actuator_obj = requests.get('http://192.168.1.2:5000/dimmers/' + str(node_id) + '/get_level').json()
+    node_id = request.args.get('nodeid', 0)
+    actuator_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/dimmers/' + str(node_id) + '/get_level').json()
     node = {
         'id': node_id,
         'actuator': actuator_obj
     }
-    if request.method == "POST":
+    if request.method == 'POST':
         new_level_val = request.form['actuatorLevel']
         requests.post(
-            'http://192.168.1.2:5000/dimmers/set_level',
+            'http://' + server_ip + ':' + str(server_port) + '/dimmers/set_level',
             json={
-                "node_id": node_id,
-                "value": new_level_val
+                'node_id': node_id,
+                'value': new_level_val
             }
         )
         node['actuator']['value'] = new_level_val
 
-    return render_template("actuatorLevel.html", node=node)
+    return render_template('actuatorLevel.html', node=node)
 
 
 @app.route('/actuators.html')
 def actuators():
-    actuators_obj = requests.get('http://192.168.1.2:5000/dimmers/get_dimmers_list').json()
-    return render_template("actuators.html", nodes=actuators_obj, nodes_len=len(actuators_obj))
+    actuators_obj = requests.get('http://' + server_ip + ':' + str(server_port) + '/dimmers/get_dimmers_list').json()
+    return render_template('actuators.html', nodes=actuators_obj, nodes_len=len(actuators_obj))
 
 
 @app.route('/networkEditConfig.html')
 def network_edit_config():
-    return render_template("networkEditConfig.html")
+    return render_template('networkEditConfig.html')
 
 
 @app.route('/404.html')
 def page404():
-    return render_template("404.html")
+    return render_template('404.html')
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html"), 404
+    return render_template('404.html'), 404
 
 
 @app.route('/network/get_nodes_configuration')
 def get_nodes_configuration():
-    nodes_config = requests.get('http://192.168.1.2:5000/network/get_nodes_configuration').json()
+    nodes_config = requests.get('http://' + server_ip + ':' + str(server_port) + '/network/get_nodes_configuration').json()
     return jsonify(nodes_config)
 
 
